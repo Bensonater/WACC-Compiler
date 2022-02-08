c5f410683797c63440221d1f88f828e62f037577
Benson Zhou <me@bensonzhou.dev>
2022-02-08 19:58:35 +0000

    feat: symbol table is now open instead of abstract

--- src/main/kotlin/frontend/ast/ASTNode.kt
+++ src/main/kotlin/frontend/ast/ASTNode.kt
@@ -4,7 +4,7 @@ import frontend.SymbolTable
 import org.antlr.v4.runtime.ParserRuleContext
 
 abstract class ASTNode(ctx: ParserRuleContext) {
-    abstract var symbolTable: SymbolTable
+    open var symbolTable = SymbolTable()
 
     open fun check(symbolTable: SymbolTable): Boolean {
         return true
