3be53c9b76cfeb7ad60832f7d8e4c7d5ffc19815
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-02-08 20:11:04 +0000

    feat: Created NewPairAST, ExprAST, CallAST nodes templates [Vincent, Sipan]

---
+++ src/main/kotlin/frontend/ast/ExprAST.kt
@@ -0,0 +1,4 @@
+package frontend.ast
+
+class ExprAST {
+}
---
+++ src/main/kotlin/frontend/ast/NewPairAST.kt
@@ -0,0 +1,7 @@
+package frontend.ast
+
+import org.antlr.v4.runtime.ParserRuleContext
+
+class NewPairAST (ctx: ParserRuleContext, fst:ExprAST, snd:ExprAST) : ASTNode(ctx) {
+
+}
