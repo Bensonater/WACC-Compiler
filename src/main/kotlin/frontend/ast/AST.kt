11ed8fb6e3395ca99695be11894bbb76feba8d50
Benson Zhou <me@bensonzhou.dev>
2022-02-08 15:12:41 +0000

    feat: initialised Symbol Table and added fields for AST

--- src/main/kotlin/frontend/ast/AST.kt
+++ src/main/kotlin/frontend/ast/AST.kt
@@ -1,6 +1,13 @@
 package frontend.ast
+import frontend.SymbolTable
+import org.antlr.v4.runtime.ParserRuleContext
 
 class AST {
+
+    lateinit var context: ParserRuleContext
+    lateinit var symbol_table: SymbolTable
+
+
 
 
 }
