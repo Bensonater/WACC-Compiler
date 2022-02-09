9aed633b45a2bf87bb092ae908f70a9fe4502a43
milesapnash <mapnash@gmail.com>
2022-02-09 16:34:52 +0000

    feat: IdentAST check implemented

--- src/main/kotlin/frontend/ast/IdentAST.kt
+++ src/main/kotlin/frontend/ast/IdentAST.kt
@@ -3,5 +3,11 @@ package frontend.ast
 import org.antlr.v4.runtime.ParserRuleContext
 
 class IdentAST(val ctx: ParserRuleContext, val name: String) : ASTNode(ctx) {
-
+    override fun check(symbolTable: SymbolTable): Boolean {
+        if (symbolTable.get(name) == null) {
+            // Return semantic error "Variable is not assigned {name}"
+            return false
+        }
+        return true
+    }
 }
