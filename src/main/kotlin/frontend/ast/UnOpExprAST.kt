a4417001c9ed770b2e6dc9d330a0d0611f48c026
Benson Zhou <me@bensonzhou.dev>
2022-02-09 18:36:08 +0000

    feat: completed check function for UnOpExprAST

--- src/main/kotlin/frontend/ast/UnOpExprAST.kt
+++ src/main/kotlin/frontend/ast/UnOpExprAST.kt
@@ -1,5 +1,9 @@
 package frontend.ast
 
+import frontend.SymbolTable
+import frontend.ast.literal.BoolLiterAST
+import frontend.ast.literal.CharLiterAST
+import frontend.ast.literal.IntLiterAST
 import org.antlr.v4.runtime.ParserRuleContext
 
 enum class UnOp {
@@ -11,5 +15,21 @@ enum class UnOp {
 }
 
 class UnOpExprAST(ctx: ParserRuleContext, val unOp: UnOp, val expr: ExprAST) : ExprAST(ctx) {
+    override fun check(symbolTable: SymbolTable): Boolean {
+        val result = when (unOp) {
+            UnOp.NOT -> expr is BoolLiterAST
+            UnOp.MINUS -> expr is IntLiterAST
+            UnOp.LEN -> expr is ArrayElemAST
+            UnOp.ORD -> expr is CharLiterAST
+            UnOp.CHR -> expr is IntLiterAST
+            else -> {
+                false
+            }
+        }
+        if (!result) {
+            // throw an error about incompatible type
+        }
+        return true
+    }
 }
 
