b18deb95bdc590af4007d9acc4e622be795fa932
Benson Zhou <me@bensonzhou.dev>
2022-02-09 12:46:18 +0000

    feat: uses hashmap to represent the symbol table

--- src/main/kotlin/frontend/SymbolTable.kt
+++ src/main/kotlin/frontend/SymbolTable.kt
@@ -1,5 +1,33 @@
 package frontend
 
+import frontend.ast.ASTNode
+
 class SymbolTable {
+    var symbolTable = HashMap<String, ASTNode>()
+    lateinit var parent: SymbolTable
+
+    fun get(name: String): ASTNode? {
+        return symbolTable[name]
+    }
+
+    fun put(name: String, astNode: ASTNode) {
+        symbolTable[name] = astNode
+    }
+
+    fun setParentTable(st: SymbolTable) {
+        parent = st
+    }
+
+    fun lookupAll(name: String): ASTNode? {
+        var st = this
+        while (st != null) {
+            var ast = st.get(name)
+            if (ast != null) {
+                return ast
+            }
+            st = st.parent
+        }
+        return null
+    }
 
 }
