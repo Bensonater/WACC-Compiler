893c91c9bdba79e9aefd4f8a71cb8fa0039635e7
Vincent <vl620@ic.ac.uk>
2022-02-10 17:37:45 +0000

    fix: Check array index dimension match actual array dimension

--- src/main/kotlin/frontend/ast/ArrayElemAST.kt
+++ src/main/kotlin/frontend/ast/ArrayElemAST.kt
@@ -2,8 +2,6 @@ package frontend.ast
 
 import frontend.SymbolTable
 import frontend.ast.type.ArrayTypeAST
-import frontend.ast.type.BaseType
-import frontend.ast.type.BaseTypeAST
 import frontend.ast.type.TypeAST
 import org.antlr.v4.runtime.ParserRuleContext
 
@@ -19,23 +17,19 @@ class ArrayElemAST(val ctx: ParserRuleContext, val ident: IdentAST, val listOfIn
             // Call semantic error "Array elem type isn't an array"
             return false
         }
-        if (listOfIndex.size > typeAST.dimension) {
-            // Call semantic error "Array index dimension exceeds actual array dimension"
+        if (listOfIndex.size != typeAST.dimension) {
+            // Call semantic error "Array index dimension doesn't match actual array dimension"
             return false
         }
         listOfIndex.forEach {
             if (!it.check(symbolTable)) {
                 return false
             }
-            if (it.getType(symbolTable) != BaseTypeAST(ctx, BaseType.INT)) {
-                // Call semantic error "Array index should be of type Int"
-                return false
-            }
         }
         return true
     }
 
-    override fun getType(symbolTable: SymbolTable): TypeAST? {
+    override fun getType(symbolTable: SymbolTable): TypeAST {
         val typeAST = ident.getType(symbolTable) as ArrayTypeAST
         return if (typeAST.dimension > listOfIndex.size) {
             ArrayTypeAST(ctx, typeAST.type, typeAST.dimension - listOfIndex.size)
