f7ed01a66b499da8877be317af31714f3aa8cae1
Vincent <vl620@ic.ac.uk>
2022-02-10 16:54:40 +0000

    feat: Check array index type should be int and doesn't exceed dimension in ArrayElemAST

--- src/main/kotlin/frontend/ast/ArrayElemAST.kt
+++ src/main/kotlin/frontend/ast/ArrayElemAST.kt
@@ -2,10 +2,37 @@ package frontend.ast
 
 import frontend.SymbolTable
 import frontend.ast.type.ArrayTypeAST
+import frontend.ast.type.BaseType
+import frontend.ast.type.BaseTypeAST
 import frontend.ast.type.TypeAST
 import org.antlr.v4.runtime.ParserRuleContext
 
 class ArrayElemAST(val ctx: ParserRuleContext, val ident: IdentAST, val listOfIndex: List<ExprAST>) : ExprAST(ctx) {
+
+    override fun check(symbolTable: SymbolTable): Boolean {
+        if (!ident.check(symbolTable)) {
+            return false
+        }
+        val typeAST = ident.getType(symbolTable)
+        if (typeAST !is ArrayTypeAST) {
+            // Call semantic error "Array elem type isn't an array"
+            return false
+        }
+        if (listOfIndex.size > typeAST.dimension) {
+            // Call semantic error "Array index dimension exceeds actual array dimension"
+            return false
+        }
+        listOfIndex.forEach {
+            if (!it.check(symbolTable)) {
+                return false
+            }
+            if (it.getType(symbolTable) != BaseTypeAST(ctx, BaseType.INT)) {
+                // Call semantic error "Array index should be of type Int"
+                return false
+            }
+        }
+        return true
+    }
 
     override fun getType(symbolTable: SymbolTable): TypeAST? {
         val typeAST = ident.getType(symbolTable) as ArrayTypeAST
@@ -14,20 +41,5 @@ class ArrayElemAST(val ctx: ParserRuleContext, val ident: IdentAST, val listOfIn
         } else {
             typeAST.type
         }
-    }
-
-    override fun check(symbolTable: SymbolTable): Boolean {
-        if (!ident.check(symbolTable)) {
-            return false
-        }
-
-        val typeAST = ident.getType(symbolTable) as ArrayTypeAST
-        listOfIndex.forEach {
-            if (it.getType(symbolTable) != typeAST.type) {
-                // raise an error here about invalid type
-                return false
-            }
-        }
-        return true
     }
 }
