4adaafadb7945cb451cd822e391aeeb345e553ab
milesapnash <mapnash@gmail.com>
2022-02-11 03:48:11 +0000

    test: Added tests to ensure no errors are returned for valid WACC example files

--- src/test/kotlin/frontend/ParserTest.kt
+++ src/test/kotlin/frontend/ParserTest.kt
@@ -27,7 +27,10 @@ class ParserTest {
         val tokens = CommonTokenStream(lexer)
         val parser = WACCParser(tokens)
         val tree = parser.program() // begin parsing at program rule
-        assertEquals("(program begin (stat print (expr (strLiter \"lorem ipsum\"))) end <EOF>)", tree.toStringTree(parser))
+        assertEquals(
+            "(program begin (stat print (expr (strLiter \"lorem ipsum\"))) end <EOF>)",
+            tree.toStringTree(parser)
+        )
     }
 
     @Test
@@ -37,37 +40,47 @@ class ParserTest {
         val tokens = CommonTokenStream(lexer)
         val parser = WACCParser(tokens)
         val tree = parser.program() // begin parsing at program rule
-        assertEquals("(program begin (stat (type (baseType int)) (ident i) = " +
-                "(assignRhs (expr (intLiter 10)))) end <EOF>)", tree.toStringTree(parser))
+        assertEquals(
+            "(program begin (stat (type (baseType int)) (ident i) = " +
+                    "(assignRhs (expr (intLiter 10)))) end <EOF>)", tree.toStringTree(parser)
+        )
     }
 
     @Test
     fun parserReturnsSequenceTree() {
-        val input = CharStreams.fromString("begin\n" +
-                "    int i = 10;\n" +
-                "    print \"lorem ipsum\"\n" +
-                "end")
+        val input = CharStreams.fromString(
+            "begin\n" +
+                    "    int i = 10;\n" +
+                    "    print \"lorem ipsum\"\n" +
+                    "end"
+        )
         val lexer = WACCLexer(input)
         val tokens = CommonTokenStream(lexer)
         val parser = WACCParser(tokens)
         val tree = parser.program() // begin parsing at program rule
-        assertEquals("(program begin (stat (stat (type (baseType int)) (ident i) = " +
-                "(assignRhs (expr (intLiter 10)))) ; (stat print (expr (strLiter \"lorem ipsum\")))) end <EOF>)",
-            tree.toStringTree(parser))
+        assertEquals(
+            "(program begin (stat (stat (type (baseType int)) (ident i) = " +
+                    "(assignRhs (expr (intLiter 10)))) ; (stat print (expr (strLiter \"lorem ipsum\")))) end <EOF>)",
+            tree.toStringTree(parser)
+        )
     }
 
     @Test
     fun parserReturnsCorrectOrderOfOperationsTree() {
-        val input = CharStreams.fromString("begin\n" +
-                "    int i = 10 + 5 * 6 - 2\n" +
-                "end")
+        val input = CharStreams.fromString(
+            "begin\n" +
+                    "    int i = 10 + 5 * 6 - 2\n" +
+                    "end"
+        )
         val lexer = WACCLexer(input)
         val tokens = CommonTokenStream(lexer)
         val parser = WACCParser(tokens)
         val tree = parser.program() // begin parsing at program rule
-        assertEquals("(program begin (stat (type (baseType int)) (ident i) = (assignRhs" +
-                " (expr (expr (expr (intLiter 10)) (binaryOper2 +) (expr " +
-                "(expr (intLiter 5)) (binaryOper1 *) (expr (intLiter 6)))) (binaryOper2 -) " +
-                "(expr (intLiter 2))))) end <EOF>)", tree.toStringTree(parser))
+        assertEquals(
+            "(program begin (stat (type (baseType int)) (ident i) = (assignRhs" +
+                    " (expr (expr (expr (intLiter 10)) (binaryOper2 +) (expr " +
+                    "(expr (intLiter 5)) (binaryOper1 *) (expr (intLiter 6)))) (binaryOper2 -) " +
+                    "(expr (intLiter 2))))) end <EOF>)", tree.toStringTree(parser)
+        )
     }
 }
