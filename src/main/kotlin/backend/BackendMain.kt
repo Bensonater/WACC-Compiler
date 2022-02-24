965ef58f29c6e7ee74b31aab98bb8410df591ae3
Vincent <vl620@ic.ac.uk>
2022-02-24 21:41:34 +0000

    refactor: Refactor assembly write text from BackendMain into Main

--- src/main/kotlin/backend/BackendMain.kt
+++ src/main/kotlin/backend/BackendMain.kt
@@ -3,7 +3,7 @@ package backend
 import frontend.ast.ASTNode
 import frontend.ast.ProgramAST
 
-fun main(ast: ASTNode) {
+fun main(ast: ASTNode): String {
     val instructions = GenerateASTVisitor(ProgramState()).visit(ast as ProgramAST)
     return printCode(instructions!!)
 }
