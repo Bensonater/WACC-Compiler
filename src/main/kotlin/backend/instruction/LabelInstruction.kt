f9cb85e9369d67b470c92942866e870241a007d7
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-02-24 13:45:10 +0000

    feat: Began implementing MessageLabel

--- src/main/kotlin/backend/instruction/LabelInstruction.kt
+++ src/main/kotlin/backend/instruction/LabelInstruction.kt
@@ -6,6 +6,13 @@ abstract class LabelInstruction (private val labelName : String) : Instruction {
     }
 }
 
-class GeneralLabel(labelName: String) : LabelInstruction(labelName)
+data class GeneralLabel(private val labelName: String) : LabelInstruction(labelName)
 
-class FunctionLabel(functionName: String) : LabelInstruction("f_$functionName")
+data class FunctionLabel(private val functionName: String) : LabelInstruction("f_$functionName")
+
+data class MessageLabel(val index: Int, val msg: String) : LabelInstruction(msg) {
+    override fun toString(): String {
+        TODO("Implement message label")
+    }
+
+}
