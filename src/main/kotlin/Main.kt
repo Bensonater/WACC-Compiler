cce6b843c434fff93b79cacb60f3ccebbe56e8d3
Benson Zhou <me@bensonzhou.dev>
2022-02-24 21:45:24 +0000

    fix: assembly file is now generated at the source of the repo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,7 +24,7 @@ fun main(args: Array<String>) {
     val code = backend.main(ast)
 
     // Creates an assembly file and write the instructions
-    val fileName = args[0].split(".wacc")[0]
+    val fileName = args[0].split(".wacc")[0].split("/").last()
     val file = File("$fileName.s")
     file.writeText(code)
 
