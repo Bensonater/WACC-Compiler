7341d42338f29176411f600835113360b686e4a1
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-02-28 18:23:12 +0000

    fix: Added Condition to Load instructions in visitor

--- src/main/kotlin/backend/GenerateASTVisitor.kt
+++ src/main/kotlin/backend/GenerateASTVisitor.kt
@@ -4,6 +4,7 @@ import backend.addressingmodes.ImmediateInt
 import backend.enums.Register
 import backend.addressingmodes.ImmediateIntOperand
 import backend.addressingmodes.RegisterOperand
+import backend.enums.Condition
 import backend.instruction.*
 import frontend.SymbolTable
 import frontend.ast.*
@@ -77,7 +78,7 @@ class GenerateASTVisitor (val programState: ProgramState) {
         deallocateStack(stackOffset, instructions)
 
 
-        instructions.add(LoadInstruction(ImmediateInt(0), Register.R0))
+        instructions.add(LoadInstruction(Condition.AL, ImmediateInt(0), Register.R0))
         instructions.add(EndInstruction())
         instructions.add(DirectiveInstruction("ltorg"))
 
