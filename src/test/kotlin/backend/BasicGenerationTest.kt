a24569ae2386c2b5db7a87f231e21c5784d11870
Miles Nash <mn820@ic.ac.uk>
2022-02-28 18:44:56 +0000

    test: Attempted fix to backend testing

--- src/test/kotlin/backend/BasicGenerationTest.kt
+++ src/test/kotlin/backend/BasicGenerationTest.kt
@@ -8,33 +8,18 @@ import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
 class BasicGenerationTest {
-    val root = "wacc_examples/valid/basic/exit/exitBasic.wacc"
+    val root = "wacc_examples/valid/basic/skip/skip.wacc"
 
     @Test
     fun assemblyIsFunctionallyCorrect() {
-        val input = CharStreams.fromFileName(root)
-
-        val astStatusPair = frontend.main(input)
-        if (astStatusPair.first != SUCCESS_CODE) {
-            exitProcess(astStatusPair.first)
+        Runtime.getRuntime().exec("./compile $root")
+        Runtime.getRuntime().exec("arm-linux-gnueabi-gcc -o skip -mcpu=arm1176jzf-s -mtune=arm1176jzf-s skip.s")
+        val process2 = Runtime.getRuntime().exec("qemu-arm -L /usr/arm-linux-gnueabi skip")
+        process2.inputStream.reader(Charsets.UTF_8).use {
+            println(it.readText())
         }
-
-        val ast = astStatusPair.second!!
-
-        val code = main(ast)
-
-//        // Creates an assembly file and write the instructions
-//        val fileName = root.split(".wacc")[0].split("/").last()
-//        val file = File("$fileName.s")
-//        file.writeText(code)
-//
-//        Runtime.getRuntime().exec("arm-linux-gnueabi-gcc -o exitBasic -mcpu=arm1176jzf-s -mtune=arm1176jzf-s exitBasic.s")
-//        val process2 = Runtime.getRuntime().exec("qemu-arm -L /usr/arm-linux-gnueabi exitBasic")
-//        process2.inputStream.reader(Charsets.UTF_8).use {
-//            println(it.readText())
-//        }
-//        process2.waitFor(5, TimeUnit.SECONDS)
-//        println(process2.exitValue())
+        process2.waitFor(5, TimeUnit.SECONDS)
+        println(process2.exitValue())
     }
 
 }
