589c7517897116f639da43603100edb43e53cb6a
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-02-28 15:41:39 +0000

    feat: Added callee regs helpers to ProgramState

--- src/main/kotlin/backend/ProgramState.kt
+++ src/main/kotlin/backend/ProgramState.kt
@@ -18,4 +18,20 @@ class ProgramState {
         Register.R6, Register.R7, Register.R8, Register.R9, Register.R10, Register.R11, Register.R12))
     var inUseCalleeSavedRegs: ArrayDeque<Register> = ArrayDeque<Register>()
 
+
+    fun freeAllCalleeRegs() {
+        while (inUseCalleeSavedRegs.isNotEmpty()) {
+            freeCalleeReg()
+        }
+    }
+
+    /**
+     * Free most recently used callee reg.
+     */
+    fun freeCalleeReg() {
+        if (inUseCalleeSavedRegs.isEmpty()) {
+            return
+        }
+        freeCalleeSavedRegs.push(inUseCalleeSavedRegs.pop())
+    }
 }
