8c0f13db3acf9d48ca7869371f2eeec0d0b526e5
Vincent <vl620@ic.ac.uk>
2022-03-03 13:37:10 +0000

    fix: Fix check BaseType in visitReadAST

--- src/main/kotlin/backend/GenerateASTVisitor.kt
+++ src/main/kotlin/backend/GenerateASTVisitor.kt
@@ -461,12 +461,12 @@ class GenerateASTVisitor (val programState: ProgramState) {
         instructions.add(MoveInstruction(Condition.AL, Register.R0, RegisterOperand(Register.R4)))
 
         /** Adds specific calls to read library functions */
-        when (ast.assignLhs) {
-            is IntLiterAST -> {
+        when ((ast.assignLhs as BaseTypeAST).type) {
+            BaseType.INT -> {
                 instructions.add(BranchInstruction(Condition.AL, GeneralLabel(CallFunc.READ_INT.toString()), true))
                 ProgramState.library.addCode(CallFunc.READ_INT)
             }
-            is CharLiterAST -> {
+            BaseType.CHAR -> {
                 instructions.add(BranchInstruction(Condition.AL, GeneralLabel(CallFunc.READ_CHAR.toString()), true))
                 ProgramState.library.addCode(CallFunc.READ_CHAR)
             }
