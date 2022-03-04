24324d3553678ef12ac2033b2b743b8e189d6ce9
Vincent <vl620@ic.ac.uk>
2022-03-04 16:04:39 +0000

    fix: Remove stack pointer changes in PushInstruction.kt and PopInstruction.kt

--- src/main/kotlin/backend/instruction/PopInstruction.kt
+++ src/main/kotlin/backend/instruction/PopInstruction.kt
@@ -1,14 +1,8 @@
 package backend.instruction
 
-import backend.ProgramState
-import backend.SIZE_OF_POINTER
 import backend.enums.Register
 
 class PopInstruction (private val register : Register) : Instruction {
-    init {
-        ProgramState.stackPointer -= SIZE_OF_POINTER
-    }
-
     override fun toString(): String {
         return "POP {$register}"
     }
--- src/main/kotlin/backend/instruction/PushInstruction.kt
+++ src/main/kotlin/backend/instruction/PushInstruction.kt
@@ -1,14 +1,8 @@
 package backend.instruction
 
-import backend.ProgramState
-import backend.SIZE_OF_POINTER
 import backend.enums.Register
 
 class PushInstruction (private val register : Register) : Instruction {
-    init {
-        ProgramState.stackPointer += SIZE_OF_POINTER
-    }
-
     override fun toString(): String {
         return "PUSH {$register}"
     }
