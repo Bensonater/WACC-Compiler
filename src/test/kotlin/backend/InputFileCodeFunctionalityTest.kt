ae1ae0ab57a0ec884d4d0ab1a250c60a243ad93b
milesapnash <mapnash@gmail.com>
2022-03-04 04:34:05 +0000

    test: Simplified code for integration tests

--- src/test/kotlin/backend/InputFileCodeFunctionalityTest.kt
+++ src/test/kotlin/backend/InputFileCodeFunctionalityTest.kt
@@ -9,56 +9,52 @@ import kotlin.test.assertEquals
 class InputFileCodeFunctionalityTest : TestUtils {
     @Test
     fun assemblyIsFunctionallyCorrect() {
-        val root = "wacc_examples/valid/inputFiles"
         val map = mapOutputsAndErrorCodes()
         var testsPassed = 0
         var testsRan = 0
 
-        doForEachFile(File(root)) { file ->
+        doForEachFile(File("wacc_examples/valid/inputFiles")) { file ->
             val name = file.nameWithoutExtension
-            val path = file.invariantSeparatorsPath
-            var process = ProcessBuilder("./compile", path).start()
             val refOutput = map[name]!!.first
             val refError = map[name]!!.second
 
-            process.waitFor()
-            process =
-                ProcessBuilder(
-                    "arm-linux-gnueabi-gcc",
-                    "-o",
-                    name,
-                    "-mcpu=arm1176jzf-s",
-                    "-mtune=arm1176jzf-s",
-                    "$name.s"
-                ).start()
-            process.waitFor()
+            ProcessBuilder("./compile", file.invariantSeparatorsPath).start()
+                .waitFor(5, TimeUnit.SECONDS)
+            ProcessBuilder(
+                "arm-linux-gnueabi-gcc",
+                "-o",
+                name,
+                "-mcpu=arm1176jzf-s",
+                "-mtune=arm1176jzf-s",
+                "$name.s"
+            ).start().waitFor(5, TimeUnit.SECONDS)
+
+            var output: String
             val inputFile = File("reference_output/$name/input.txt")
-            process =
+            val process =
                 ProcessBuilder("qemu-arm", "-L", "/usr/arm-linux-gnueabi", name).redirectInput(
                     inputFile
                 ).start()
             process.waitFor(5, TimeUnit.SECONDS)
 
-            var output: String
             process.inputStream.reader(Charsets.UTF_8).use {
                 output = it.readText()
             }
 
             if ((refOutput == output) && (refError == process.exitValue())) {
-                println("PASSED $name")
+                println("- PASSED $name")
                 testsPassed++
             } else {
-                println("FAILING $name")
-                println("REFERENCE EC: $refError")
-                println("OUR       EC: ${process.exitValue()}")
-                println("------REFERENCE------")
+                println("- FAILING $name -")
+                println("------REFERENCE OUTPUT------")
                 println(refOutput)
-                println("------OUR------------")
+                println("------OUR OUTPUT------")
                 println(output)
+                println("REFERENCE ERROR CODE: $refError")
+                println("OUR ERROR CODE: ${process.exitValue()}")
             }
 
-            process = ProcessBuilder("rm", "$name.s", name).start()
-            process.waitFor()
+            ProcessBuilder("rm", "$name.s", name).start().waitFor(5, TimeUnit.SECONDS)
             testsRan++
         }
         assertEquals(testsRan, testsPassed)
