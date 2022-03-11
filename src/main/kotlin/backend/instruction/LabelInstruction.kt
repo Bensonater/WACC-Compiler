2d14863592769fd58d0b33d52c528ab53ab6de97
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:58:11 +0000

    feat: Added x86_64 for LabelInstruction.kt

--- src/main/kotlin/backend/instruction/LabelInstruction.kt
+++ src/main/kotlin/backend/instruction/LabelInstruction.kt
@@ -1,8 +1,14 @@
 package backend.instruction
 
+import backend.Language
+import language
+
 abstract class LabelInstruction (val labelName : String) : Instruction {
     override fun toString(): String {
-        return "$labelName:"
+        return when (language){
+            Language.ARM -> "$labelName:"
+            Language.X86_64 -> "$labelName:"
+        }
     }
 }
 
@@ -12,11 +18,21 @@ class FunctionLabel(functionName: String) : LabelInstruction("f_$functionName")
 
 data class MessageLabel(val index: Int, val msg: String) : LabelInstruction(msg) {
     override fun toString(): String {
-        val stringInstructions = mutableListOf<String>()
-        stringInstructions.add(GeneralLabel("msg_$index").toString())
-        stringInstructions.add("\t${DirectiveInstruction("word ${msg.length - msg.count { c -> c == '\\' }}")}")
-        stringInstructions.add("\t${DirectiveInstruction("ascii \"${msg}\"")}")
-        return stringInstructions.joinToString(separator = "\n")
+        return when (language) {
+            Language.ARM -> {
+                val stringInstructions = mutableListOf<String>()
+                stringInstructions.add(GeneralLabel("msg_$index").toString())
+                stringInstructions.add("\t${DirectiveInstruction("word ${msg.length - msg.count { c -> c == '\\' }}")}")
+                stringInstructions.add("\t${DirectiveInstruction("ascii \"${msg}\"")}")
+                stringInstructions.joinToString(separator = "\n")
+            }
+            Language.X86_64 -> {
+                val stringInstructions = mutableListOf<String>()
+                stringInstructions.add(GeneralLabel("msg_$index").toString())
+                stringInstructions.add("\t ${DirectiveInstruction("string \"$msg\"")}")
+                stringInstructions.joinToString(separator = "\n")
+            }
+        }
     }
 
 }
