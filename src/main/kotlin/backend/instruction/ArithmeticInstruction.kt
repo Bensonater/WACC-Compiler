a248edb84dd1496057b186986a76a0a94d71cf1f
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:55:47 +0000

    feat: Added x86_64 for ArithmeticInstruction.kt

--- src/main/kotlin/backend/instruction/ArithmeticInstruction.kt
+++ src/main/kotlin/backend/instruction/ArithmeticInstruction.kt
@@ -1,17 +1,30 @@
 package backend.instruction
 
+import backend.Language
 import backend.enums.Register
 import backend.addressingmodes.AddressingMode
+import language
 
 enum class ArithmeticInstrType {
     ADD,
     SUB,
-    RSB
+    RSB;
+
+    override fun toString(): String {
+        return when (language) {
+            Language.ARM -> name
+            Language.X86_64 -> name.lowercase()
+        }
+    }
 }
 
 
 class ArithmeticInstruction (val type: ArithmeticInstrType, val reg1: Register, val reg2: Register, val operand: AddressingMode, val update: Boolean = false) : Instruction {
     override fun toString(): String {
-        return "${type.name}${if (update) "S" else ""} $reg1, $reg2, $operand"
+        return when (language) {
+            Language.ARM -> "$type${if (update) "S" else ""} $reg1, $reg2, $operand"
+            Language.X86_64 -> "$type $operand, $reg1"
+        }
+
     }
 }
