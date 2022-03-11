aa9b86e926d517608dc57f7437632f39a7cae0fe
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:58:42 +0000

    feat: Added x86_64 for MoveInstruction.kt

--- src/main/kotlin/backend/instruction/MoveInstruction.kt
+++ src/main/kotlin/backend/instruction/MoveInstruction.kt
@@ -1,12 +1,17 @@
 package backend.instruction
 
+import backend.Language
 import backend.addressingmodes.AddressingMode
 import backend.enums.Condition
 import backend.enums.Register
+import language
 
 class MoveInstruction (val condition: Condition, val reg: Register, val value: AddressingMode) : Instruction {
     override fun toString(): String {
-        return "MOV$condition $reg, $value"
+        return when (language) {
+            Language.ARM -> "MOV$condition $reg, $value"
+            Language.X86_64 -> "move $value, $reg"
+        }
     }
 
 }
