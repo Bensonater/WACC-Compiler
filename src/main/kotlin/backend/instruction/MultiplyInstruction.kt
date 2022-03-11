b5b39e9a4e91e7f4faeaa5e3fc29be2238997c96
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:58:49 +0000

    feat: Added x86_64 for MultiplyInstruction.kt

--- src/main/kotlin/backend/instruction/MultiplyInstruction.kt
+++ src/main/kotlin/backend/instruction/MultiplyInstruction.kt
@@ -1,11 +1,17 @@
 package backend.instruction
 
+import backend.Language
+import backend.addressingmodes.AddressingMode
 import backend.enums.Condition
 import backend.enums.Register
+import language
 
 class MultiplyInstruction (val condition: Condition, val rdLo: Register, val rdHi: Register,
-                           val rn: Register, val rm: Register) : Instruction {
+                           val rn: Register, val rm: Register, val operand: AddressingMode? = null) : Instruction {
     override fun toString(): String {
-        return "SMULL$condition $rdLo, $rdHi, $rn, $rm"
+        return when (language) {
+            Language.ARM -> "SMULL$condition $rdLo, $rdHi, $rn, $rm"
+            Language.X86_64 -> "imul  $operand, $rdLo"
+        }
     }
 }
