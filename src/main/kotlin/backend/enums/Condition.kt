7b4b200aa0fd425850a7487a3175c88938fddc71
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:56:15 +0000

    feat: Added x86_64 for Condition.kt

--- src/main/kotlin/backend/enums/Condition.kt
+++ src/main/kotlin/backend/enums/Condition.kt
@@ -1,5 +1,8 @@
 package backend.enums
 
+import backend.Language
+import language
+
 enum class Condition {
     EQ,
     NE,
@@ -12,6 +15,27 @@ enum class Condition {
     AL;
 
     override fun toString(): String {
-        return if (this == AL) "" else name
+        return when (language) {
+            Language.ARM -> {
+                if (this == AL) {
+                    ""
+                } else {
+                    name
+                }
+            }
+            Language.X86_64 -> {
+                when (this) {
+                    EQ -> "e"
+                    NE -> "ne"
+                    GT -> "g"
+                    GE -> "ge"
+                    LT -> "l"
+                    LE -> "le"
+                    CS -> "ae"
+                    VS -> "o"
+                    AL -> ""
+                }
+            }
+        }
     }
 }
