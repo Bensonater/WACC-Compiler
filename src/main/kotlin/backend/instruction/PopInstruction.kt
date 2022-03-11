daeddb9f97fe4598e9f7169eabbf232a4308f4a7
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:59:04 +0000

    feat: Added x86_64 for Pop and Push instructions

--- src/main/kotlin/backend/instruction/PopInstruction.kt
+++ src/main/kotlin/backend/instruction/PopInstruction.kt
@@ -1,9 +1,14 @@
 package backend.instruction
 
+import backend.Language
 import backend.enums.Register
+import language
 
 class PopInstruction (private val register : Register) : Instruction {
     override fun toString(): String {
-        return "POP {$register}"
+        return when (language) {
+            Language.ARM -> "POP {$register}"
+            Language.X86_64 -> "popq $register"
+        }
     }
 }
--- src/main/kotlin/backend/instruction/PushInstruction.kt
+++ src/main/kotlin/backend/instruction/PushInstruction.kt
@@ -1,9 +1,14 @@
 package backend.instruction
 
+import backend.Language
 import backend.enums.Register
+import language
 
 class PushInstruction (private val register : Register) : Instruction {
     override fun toString(): String {
-        return "PUSH {$register}"
+        return when (language) {
+            Language.ARM -> "PUSH {$register}"
+            Language.X86_64 -> "pushq $register"
+        }
     }
 }
