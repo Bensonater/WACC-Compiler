a0575f6cf9194304e070d4f4f2b8e844a4800f34
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:56:35 +0000

    feat: Added x86_64 for EndInstruction.kt

--- src/main/kotlin/backend/instruction/EndInstruction.kt
+++ src/main/kotlin/backend/instruction/EndInstruction.kt
@@ -1,10 +1,15 @@
 package backend.instruction
 
+import backend.Language
 import backend.enums.Register
+import language
 
 class EndInstruction : Instruction {
 
     override fun toString(): String {
-        return "POP {${Register.PC}}"
+        return when (language) {
+            Language.ARM -> "POP {${Register.PC}}"
+            Language.X86_64 -> "leave\n  ret"
+        }
     }
 }
