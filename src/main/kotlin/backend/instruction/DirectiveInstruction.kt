5c62128e55ad77f742eb66d0f73ee7703d10137f
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:56:22 +0000

    feat: Added x86_64 for DirectiveInstruction.kt

--- src/main/kotlin/backend/instruction/DirectiveInstruction.kt
+++ src/main/kotlin/backend/instruction/DirectiveInstruction.kt
@@ -1,7 +1,13 @@
 package backend.instruction
 
+import backend.Language
+import language
+
 class DirectiveInstruction (private val directive: String) : Instruction{
     override fun toString(): String {
-        return ".$directive"
+        return when (language) {
+            Language.ARM -> ".$directive"
+            Language.X86_64 -> ".$directive"
+        }
     }
 }
