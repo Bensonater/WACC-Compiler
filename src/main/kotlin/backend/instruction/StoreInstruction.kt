1c8ae66931efb924e954ddf4d9aba82564192660
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:59:15 +0000

    feat: Added x86_64 for StoreInstruction.kt

--- src/main/kotlin/backend/instruction/StoreInstruction.kt
+++ src/main/kotlin/backend/instruction/StoreInstruction.kt
@@ -1,11 +1,16 @@
 package backend.instruction
 
+import backend.Language
 import backend.addressingmodes.AddressingMode
 import backend.enums.Memory
 import backend.enums.Register
+import language
 
 class StoreInstruction (val mode: AddressingMode, val reg: Register, val memory: Memory? = null): Instruction {
     override fun toString(): String {
-        return "STR${memory?.name ?: ""} $reg, $mode"
+        return when (language) {
+            Language.ARM -> "STR${memory?.name ?: ""} $reg, $mode"
+            Language.X86_64 -> "mov $reg, $mode"
+        }
     }
 }
