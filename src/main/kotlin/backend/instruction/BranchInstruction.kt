056dfc41bb3e8dde356cf30e6f29386d4c82cb46
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:55:57 +0000

    feat: Added x86_64 for BranchInstruction.kt

--- src/main/kotlin/backend/instruction/BranchInstruction.kt
+++ src/main/kotlin/backend/instruction/BranchInstruction.kt
@@ -1,9 +1,14 @@
 package backend.instruction
 
+import backend.Language
 import backend.enums.Condition
+import language
 
 class BranchInstruction (val condition: Condition, val label: LabelInstruction, val link: Boolean): Instruction {
     override fun toString(): String {
-        return "B" + (if(link) "L" else "")  + condition.toString() + " " + label.labelName
+        return when (language) {
+            Language.ARM ->  "B" + (if (link) "L" else "") + condition.toString() + " " + label.labelName
+            Language.X86_64 -> "${if (condition == Condition.AL) "jmp" else "j$condition"} ${label.labelName}"
+        }
     }
 }
