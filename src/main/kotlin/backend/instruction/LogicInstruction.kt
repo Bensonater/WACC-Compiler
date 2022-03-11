1e822574383040e24ac9fed22e01339e05fa265f
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:58:31 +0000

    feat: Added x86_64 for LogicInstruction.kt

--- src/main/kotlin/backend/instruction/LogicInstruction.kt
+++ src/main/kotlin/backend/instruction/LogicInstruction.kt
@@ -1,7 +1,9 @@
 package backend.instruction
 
+import backend.Language
 import backend.addressingmodes.AddressingMode
 import backend.enums.Register
+import language
 
 enum class LogicOperation {
     AND,
@@ -12,6 +14,9 @@ enum class LogicOperation {
 class LogicInstruction(val op: LogicOperation, val reg1: Register, val reg2: Register,
                        val operand: AddressingMode) : Instruction {
     override fun toString(): String {
-        return "${op.name} $reg1, $reg2, $operand"
+        return when (language) {
+            Language.ARM -> "${op.name} $reg1, $reg2, $operand"
+            Language.X86_64 -> "${op.name} $reg2, $operand"
+        }
     }
 }
