ff8c7afd9f22f4ba5e64eb5dc301a96b67d6a7da
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-11 21:58:20 +0000

    feat: Added x86_64 for LoadInstruction.kt

--- src/main/kotlin/backend/instruction/LoadInstruction.kt
+++ src/main/kotlin/backend/instruction/LoadInstruction.kt
@@ -1,12 +1,23 @@
 package backend.instruction
 
+import backend.Language
 import backend.addressingmodes.AddressingMode
 import backend.enums.Condition
 import backend.enums.Memory
 import backend.enums.Register
+import language
 
 class LoadInstruction (val condition: Condition, val addressingMode: AddressingMode, val register: Register, val memoryType: Memory? = null) : Instruction {
     override fun toString(): String {
-        return "LDR${memoryType?.name ?: ""}$condition $register, $addressingMode"
+        return when (language) {
+            Language.ARM -> "LDR${memoryType?.name ?: ""}$condition $register, $addressingMode"
+            Language.X86_64 -> {
+                if (condition == Condition.AL) {
+                    "mov $addressingMode, $register"
+                } else {
+                    "cmov$condition $addressingMode, $register"
+                }
+            }
+        }
     }
 }
