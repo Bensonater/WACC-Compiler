16c0454c6cd980b16c7dbd108fa895191864e382
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-13 02:18:56 +0000

    fix: Corrected spelling in MoveInstruction

--- src/main/kotlin/backend/instruction/MoveInstruction.kt
+++ src/main/kotlin/backend/instruction/MoveInstruction.kt
@@ -10,7 +10,7 @@ class MoveInstruction (val condition: Condition, val reg: Register, val value: A
     override fun toString(): String {
         return when (language) {
             Language.ARM -> "MOV$condition $reg, $value"
-            Language.X86_64 -> "move $value, $reg"
+            Language.X86_64 -> "mov $value, $reg"
         }
     }
 
