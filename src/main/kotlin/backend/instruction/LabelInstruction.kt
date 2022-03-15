17fa560ac17abee8ac7bfcc2bacaa62480893c7b
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-15 19:26:48 +0000

    feat: Message labels now use int directive for length and string for the contents for x86

--- src/main/kotlin/backend/instruction/LabelInstruction.kt
+++ src/main/kotlin/backend/instruction/LabelInstruction.kt
@@ -18,21 +18,16 @@ class FunctionLabel(functionName: String) : LabelInstruction("f_$functionName")
 
 data class MessageLabel(val index: Int, val msg: String) : LabelInstruction(msg) {
     override fun toString(): String {
-        return when (language) {
-            Language.ARM -> {
-                val stringInstructions = mutableListOf<String>()
-                stringInstructions.add(GeneralLabel("msg_$index").toString())
-                stringInstructions.add("\t${DirectiveInstruction("word ${msg.length - msg.count { c -> c == '\\' }}")}")
-                stringInstructions.add("\t${DirectiveInstruction("ascii \"${msg}\"")}")
-                stringInstructions.joinToString(separator = "\n")
-            }
-            Language.X86_64 -> {
-                val stringInstructions = mutableListOf<String>()
-                stringInstructions.add(GeneralLabel("msg_$index").toString())
-                stringInstructions.add("\t ${DirectiveInstruction("string \"$msg\"")}")
-                stringInstructions.joinToString(separator = "\n")
-            }
+        val stringInstructions = mutableListOf<String>()
+        stringInstructions.add(GeneralLabel("msg_$index").toString())
+        if (language == Language.ARM) {
+            stringInstructions.add("\t${DirectiveInstruction("word ${msg.length - msg.count { c -> c == '\\' }}")}")
+            stringInstructions.add("\t${DirectiveInstruction("ascii \"${msg}\"")}")
+        } else {
+            stringInstructions.add("\t${DirectiveInstruction("int ${msg.length - msg.count { c -> c == '\\' }}")}")
+            stringInstructions.add("\t${DirectiveInstruction("string \"${msg}\"")}")
         }
+        return stringInstructions.joinToString(separator = "\n")
     }
 
 }
