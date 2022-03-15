bbaf748c0e6e4db65fcf42445071bfd19613cee1
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-15 19:30:17 +0000

    feat: Added call to BranchInstruction.kt

--- src/main/kotlin/backend/instruction/BranchInstruction.kt
+++ src/main/kotlin/backend/instruction/BranchInstruction.kt
@@ -8,7 +8,16 @@ class BranchInstruction (val condition: Condition, val label: LabelInstruction, 
     override fun toString(): String {
         return when (language) {
             Language.ARM ->  "B" + (if (link) "L" else "") + condition.toString() + " " + label.labelName
-            Language.X86_64 -> "${if (condition == Condition.AL) "jmp" else "j$condition"} ${label.labelName}"
+            Language.X86_64 -> {
+                if (link) {
+                    return "call ${label.labelName}"
+                }
+                return if (condition == Condition.AL) {
+                    "jmp ${label.labelName}"
+                } else {
+                    "j$condition ${label.labelName}"
+                }
+            }
         }
     }
 }
