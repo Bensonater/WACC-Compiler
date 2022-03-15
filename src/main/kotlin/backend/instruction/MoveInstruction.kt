444a602715a756bcd705c8b8da6ee22ec369b261
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-15 19:25:38 +0000

    feat: Added cmove instruction

--- src/main/kotlin/backend/instruction/MoveInstruction.kt
+++ src/main/kotlin/backend/instruction/MoveInstruction.kt
@@ -3,6 +3,7 @@ package backend.instruction
 import backend.Language
 import backend.addressingmodes.AddressingMode
 import backend.enums.Condition
+import backend.enums.Memory
 import backend.enums.Register
 import language
 
@@ -12,6 +13,14 @@ class MoveInstruction (val condition: Condition, val reg: Register, val value: A
             Language.ARM -> "MOV$condition $reg, $value"
             Language.X86_64 -> "mov $value, $reg"
         }
+    }
+
+}
+
+
+class CMoveInstruction (val condition: Condition, val reg1: Register, val reg2: Register) : Instruction {
+    override fun toString(): String {
+        return "cmov$condition $reg1, $reg2"
     }
 
 }
