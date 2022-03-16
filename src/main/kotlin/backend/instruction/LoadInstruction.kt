a003fb837957df50cff2e03aa513608dc0702cbb
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-16 15:46:59 +0000

    feat: Memory type considered in load instruction

--- src/main/kotlin/backend/instruction/LoadInstruction.kt
+++ src/main/kotlin/backend/instruction/LoadInstruction.kt
@@ -11,7 +11,16 @@ class LoadInstruction (val condition: Condition, val addressingMode: AddressingM
     override fun toString(): String {
         return when (language) {
             Language.ARM -> "LDR${memoryType?.name ?: ""}$condition $register, $addressingMode"
-            Language.X86_64 -> "mov $addressingMode, $register"
+            Language.X86_64 -> {
+                var result = ""
+                if (memoryType == Memory.B || memoryType == Memory.SB) {
+                    result += "mov $0, $register\n\t"
+                }
+                result += "mov${memoryType?.name?.lowercase()?.last() ?: ""} " +
+                        "$addressingMode, ${memoryType?.getRegType(register) ?: register}"
+                return result
+            }
+
         }
     }
 }
