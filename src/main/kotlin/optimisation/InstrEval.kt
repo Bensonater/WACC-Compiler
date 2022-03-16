c6b3087c978cbb8987eff15657af2304a214457c
Benson Zhou <me@bensonzhou.dev>
2022-03-16 17:36:38 +0000

    test: tests are now running with optimisation flags refactor: structure of InstrEval

--- src/main/kotlin/optimisation/InstrEval.kt
+++ src/main/kotlin/optimisation/InstrEval.kt
@@ -1,26 +1,28 @@
 package optimisation
 
+import backend.addressingmodes.RegisterMode
 import backend.instruction.Instruction
 import backend.instruction.LoadInstruction
 import backend.instruction.StoreInstruction
 
 class InstrEval {
     fun optimise(instructions: List<Instruction>): List<Instruction> {
-        var optimised = instructions
-        return optimiseStoreThenLoad(optimised)
-    }
-
-    private fun optimiseStoreThenLoad(instructions: List<Instruction>): List<Instruction> {
-        var prev = instructions.first()
         val optimised = mutableListOf<Instruction>()
+
+        var prev = instructions.first()
         for (i in instructions) {
-            if (prev is StoreInstruction && i is LoadInstruction && prev.reg == i.register) {
+            if (storeThenLoad(prev, i)) {
             } else {
                 optimised.add(i)
             }
             prev = i
         }
         return optimised
+    }
+
+    private fun storeThenLoad(prev: Instruction, curr: Instruction): Boolean {
+        return prev is StoreInstruction && curr is LoadInstruction && prev.reg == curr.register
+                && prev.mode is RegisterMode && curr.addressingMode is RegisterMode && prev.mode.reg == curr.addressingMode.reg
     }
 
 //    fun optimiseAddZero(instructions: List<Instruction>): List<Instruction>{
