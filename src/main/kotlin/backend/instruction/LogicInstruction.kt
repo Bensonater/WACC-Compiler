ab269974f7257bbfd50b0c4f1d42b5456d40eaaf
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-16 15:47:14 +0000

    feat: Added to string for logic instruction

--- src/main/kotlin/backend/instruction/LogicInstruction.kt
+++ src/main/kotlin/backend/instruction/LogicInstruction.kt
@@ -8,15 +8,35 @@ import language
 enum class LogicOperation {
     AND,
     ORR,
-    EOR
+    EOR;
+
+    override fun toString(): String {
+        return when (language) {
+            Language.X86_64 -> {
+                when (this) {
+                    AND -> "and"
+                    ORR-> "or"
+                    EOR -> "xor"
+                }
+            }
+            Language.ARM -> this.name
+        }
+    }
 }
 
 class LogicInstruction(val op: LogicOperation, val reg1: Register, val reg2: Register,
                        val operand: AddressingMode) : Instruction {
     override fun toString(): String {
         return when (language) {
-            Language.ARM -> "${op.name} $reg1, $reg2, $operand"
-            Language.X86_64 -> "${op.name} $reg2, $operand"
+            Language.ARM -> "$op $reg1, $reg2, $operand"
+            Language.X86_64 -> {
+                var result = ""
+                if (reg1 != reg2) {
+                    result += "mov $reg2, $reg1\n\t"
+                }
+                result += "$op $operand, $reg1"
+                return result
+            }
         }
     }
 }
