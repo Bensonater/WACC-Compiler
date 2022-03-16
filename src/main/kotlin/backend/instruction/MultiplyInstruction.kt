4506d9e28598a78403c3b265c1a02bc7674ecef0
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-16 20:56:01 +0000

    feat: Added imul instruction

--- src/main/kotlin/backend/instruction/MultiplyInstruction.kt
+++ src/main/kotlin/backend/instruction/MultiplyInstruction.kt
@@ -9,9 +9,15 @@ import language
 class MultiplyInstruction (val condition: Condition, val rdLo: Register, val rdHi: Register,
                            val rn: Register, val rm: Register, val operand: AddressingMode? = null) : Instruction {
     override fun toString(): String {
-        return when (language) {
-            Language.ARM -> "SMULL$condition $rdLo, $rdHi, $rn, $rm"
-            Language.X86_64 -> "imul  $operand, $rdLo"
-        }
+        return "SMULL$condition $rdLo, $rdHi, $rn, $rm"
+    }
+}
+
+/**
+ * Multiples %rax with %{reg}
+ */
+class iMultiplyInstruction (val reg: Register) : Instruction {
+    override fun toString(): String {
+        return "imul $reg"
     }
 }
