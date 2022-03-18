deceb0dad4ab5de9c6201a74a3e6a6a609a18eb7
Miles Nash <mn820@ic.ac.uk>
2022-03-18 17:54:45 +0000

    test: Updated x86 Tests to Reflect Improved Implementation

--- src/test/kotlin/extension/X86CodeFunctionalityTest.kt
+++ src/test/kotlin/extension/X86CodeFunctionalityTest.kt
@@ -69,51 +69,26 @@ class X86CodeFunctionalityTest {
         @JvmStatic
         fun testFiles(): List<File> {
             val root = "wacc_examples/valid"
-            return getEachFile(File(root),
+            /**
+             * We have had to exclude some tests for arrays, as a different address has been used
+             * for the x86 compared to ARM architecture- they are still functionally correct. We
+             * have also excluded the pair and pointer test files
+             */
+            return getEachFile(
+                File(root),
                 listOf(
                     File("$root/array/array.wacc"),
+                    File("$root/array/arrayNested.wacc"),
                     File("$root/array/arrayPrint.wacc"),
                     File("$root/array/printRef.wacc"),
                     File("$root/function/simple_functions/functionManyArguments.wacc"),
-                    File("$root/pairs/checkRefPair.wacc"),
-                    File("$root/pairs/printPair.wacc"),
-                    File("$root/pairs/printPairOfNulls.wacc"),
-                    File("$root/scope/printAllTypes.wacc"),
-                    File("$root/pointers/charPointerArithmetic.wacc"),
-                    File("$root/pointers/intPointerArithmetic.wacc"),
-                    File("$root/pointers/pointerArray.wacc"),
-                    File("$root/pointers/pointerBasic.wacc"),
-                    File("$root/pointers/pointerBinExp.wacc"),
-                    File("$root/array/arrayNested.wacc"),
+                    File("$root/function/simple_functions/functionReturnPair.wacc"),
                     File("$root/runtimeErr/arrayOutOfBounds/arrayOutOfBounds.wacc"),
                     File("$root/runtimeErr/arrayOutOfBounds/arrayOutOfBoundsWrite.wacc"),
-                    File("$root/inputFiles/echoBigInt.wacc"),
-                    File("$root/inputFiles/echoBigNegInt.wacc"),
-                    File("$root/inputFiles/echoChar.wacc"),
-                    File("$root/inputFiles/echoInt.wacc"),
-                    File("$root/inputFiles/echoNegInt.wacc"),
-                    File("$root/inputFiles/echoPuncChar.wacc"),
-                    File("$root/inputFiles/fibonacciFullIt.wacc"),
-                    File("$root/inputFiles/fibonacciFullRec.wacc"),
-                    File("$root/inputFiles/IOLoop.wacc"),
-                    File("$root/inputFiles/IOSequence.wacc"),
-                    File("$root/inputFiles/printInputTriangle.wacc"),
-                    File("$root/inputFiles/read.wacc"),
-                    File("$root/inputFiles/readPair.wacc"),
-                    File("$root/inputFiles/rmStyleAddIO.wacc"),
-                    File("$root/function/simple_functions/functionReturnPair.wacc"),
-                    File("$root/pairs/createPair.wacc"),
-                    File("$root/pairs/createPair02.wacc"),
-                    File("$root/pairs/createPair03.wacc"),
-                    File("$root/pairs/createRefPair.wacc"),
-                    File("$root/pairs/free.wacc"),
-                    File("$root/pairs/linkedList.wacc"),
-                    File("$root/pairs/nestedPair.wacc"),
-                    File("$root/pairs/writeFst.wacc"),
-                    File("$root/pairs/writeSnd.wacc"),
-
-
-            ))
+                    File("$root/scope/printAllTypes.wacc"),
+                    File("$root/inputFiles/readPair.wacc")
+                ) + getEachFile(File("$root/pairs/")) + getEachFile(File("$root/pointers/"))
+            )
         }
     }
 }
