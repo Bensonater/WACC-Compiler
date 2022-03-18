a2944e7cb581e74bb48d88f968661ae00fc2c1ee
Sipan Petrosyan <sdp120@imperial.ac.uk>
2022-03-18 05:37:28 +0000

    feat: Added StoreRegInstruction to write the same memory type registers

--- src/main/kotlin/backend/instruction/StoreInstruction.kt
+++ src/main/kotlin/backend/instruction/StoreInstruction.kt
@@ -10,7 +10,18 @@ class StoreInstruction (val mode: AddressingMode, val reg: Register, val memory:
     override fun toString(): String {
         return when (LANGUAGE) {
             Language.ARM -> "STR${memory?.name ?: ""} $reg, $mode"
-            Language.X86_64 -> "mov${memory?.name?.lowercase()?.last() ?: ""} ${memory?.getRegType(reg) ?: reg}, $mode"
+            Language.X86_64 -> "mov${memory?.name?.lowercase()?.last() ?: ""} ${memory?.getRegType(reg) ?: reg}, " +
+                    "$mode"
+        }
+    }
+}
+
+class StoreRegInstruction (val reg1: Register, val reg2: Register, val memory: Memory? = null): Instruction {
+    override fun toString(): String {
+        return when (LANGUAGE) {
+            Language.ARM -> "STR${memory?.name ?: ""} $reg1, $reg2"
+            Language.X86_64 -> "mov${memory?.name?.lowercase()?.last() ?: ""} ${memory?.getRegType(reg1) ?: reg1}, " +
+                    "${memory?.getRegType(reg2) ?: reg2}"
         }
     }
 }
